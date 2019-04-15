74e169cde73762bb6220ab887163ff91ca7bfb8d
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2019-04-15 13:29:40 +0200

    Use es6 syntax

--- src/index.js
+++ src/index.js
@@ -1,71 +1,78 @@
-export default function QR2REGClient({ applicationId }) {
-  const API_BASE_URL = 'https://stage.qr2reg-api.vivifyideas.com';
+export default class QR2REGClient {
+  constructor({ applicationId }) {
+    this._applicationId = applicationId;
+    this._onGrantedCallbacks = [];
+    this._onDeniedCallbacks = [];
+    this._onTimedOutCallbacks = [];
+    this._onErrorCallbacks = [];
 
-  const TYPE = {
-    DENIED: 'type:denied',
-    GRANTED: 'type:granted',
-    TIMED_OUT: 'type:timed_out',
-    ERROR: 'type:error',
-  };
+    this._receiveMessage = this._receiveMessage.bind(this);
 
-  this.applicationId = applicationId;
-  this.onGrantedCallbacks = [];
-  this.onDeniedCallbacks = [];
-  this.onTimedOutCallbacks = [];
-  this.onErrorCallbacks = [];
+    window.addEventListener('message', this._receiveMessage, false);
+  }
 
-  this.signIn = () => {
+  signIn() {
     window.open(
-      `${API_BASE_URL}/api/oauth?application_id=${this.applicationId}`,
+      `${QR2REGClient._API_BASE_URL}/api/oauth?application_id=${
+        this._applicationId
+      }`,
       '_blank',
       'location=yes,left=0,top=0,height=375,width=375,status=yes'
     );
-  };
+  }
 
-  this.onGranted = onGrantedCallback => {
-    this.onGrantedCallbacks.push(onGrantedCallback);
-  };
+  onGranted(onGrantedCallback) {
+    this._onGrantedCallbacks.push(onGrantedCallback);
+  }
 
-  this.onDenied = onDeniedCallback => {
-    this.onDeniedCallbacks.push(onDeniedCallback);
-  };
+  onDenied(onDeniedCallback) {
+    this._onDeniedCallbacks.push(onDeniedCallback);
+  }
 
-  this.onTimedOut = onTimedOutCallback => {
-    this.onTimedOutCallbacks.push(onTimedOutCallback);
-  };
+  onTimedOut(onTimedOutCallback) {
+    this._onTimedOutCallbacks.push(onTimedOutCallback);
+  }
 
-  this.onError = onErrorCallback => {
-    this.onErrorCallbacks.push(onErrorCallback);
-  };
+  onError(onErrorCallback) {
+    this._onErrorCallbacks.push(onErrorCallback);
+  }
 
-  this.receiveMessage = event => {
-    if (event.origin !== API_BASE_URL) {
+  _receiveMessage(message) {
+    if (message.origin !== QR2REGClient._API_BASE_URL) {
       return;
     }
 
-    switch (event.data.type) {
-      case TYPE.GRANTED:
-        this.onGrantedCallbacks.forEach(callback =>
-          callback(event.data.authorization_code)
+    switch (message.data.type) {
+      case QR2REGClient._MESSAGE_TYPE.GRANTED:
+        this._onGrantedCallbacks.forEach(callback =>
+          callback(message.data.authorization_code)
         );
         break;
 
-      case TYPE.DENIED:
-        this.onDeniedCallbacks.forEach(callback => callback());
+      case QR2REGClient._MESSAGE_TYPE.DENIED:
+        this._onDeniedCallbacks.forEach(callback => callback());
         break;
 
-      case TYPE.TIMED_OUT:
-        this.onTimedOutCallbacks.forEach(callback => callback());
+      case QR2REGClient._MESSAGE_TYPE.TIMED_OUT:
+        this._onTimedOutCallbacks.forEach(callback => callback());
         break;
 
-      case TYPE.ERROR:
-        this.onErrorCallbacks.forEach(callback => callback(event.data.error));
+      case QR2REGClient._MESSAGE_TYPE.ERROR:
+        this._onErrorCallbacks.forEach(callback =>
+          callback(message.data.error)
+        );
         break;
 
       default:
         break;
     }
-  };
-
-  window.addEventListener('message', this.receiveMessage, false);
+  }
 }
+
+QR2REGClient._API_BASE_URL = 'https://stage.qr2reg-api.vivifyideas.com';
+QR2REGClient._MESSAGE_TYPE = {
+  DENIED: 'type:denied',
+  GRANTED: 'type:granted',
+  TIMED_OUT: 'type:timed_out',
+  ERROR: 'type:error',
+};
